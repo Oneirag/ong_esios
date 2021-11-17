007adf3c608ba89f3c74bc74981396d4341d5427
Oscar Neira <oneirag@yahoo.es>
2021-11-17 22:40:41 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='ong_esios',
+    version='0.1',
+    packages=['ong_esios'],
+    url='www.neirapinuela.es',
+    license='',
+    author='ongpi',
+    author_email='oneirag@yahoo.es',
+    description='Functions to download from from esios api',
+    install_requires=required,
+)
