1d3563400278f35e6dd4285a1e4db7dd8d3d1830
Oscar Neira <oneirag@yahoo.es>
2022-01-24 17:18:52 +0100

    Adaptation to last version of ong_utils

--- ong_esios/__init__.py
+++ ong_esios/__init__.py
@@ -1,5 +1,6 @@
-from ong_utils import OngConfig, LOCAL_TZ, http
+from ong_utils import OngConfig, LOCAL_TZ, create_pool_manager
 
+http = create_pool_manager()
 _util = OngConfig("ong_esios", cfg_filename="ong_config.yml")
 logger = _util.logger
 config = _util.config
